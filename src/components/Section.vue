3d6cd2ee7cce2791d3a90a9ecf226b800e545311
Diêgo Correia de Andrade <dgcorreia8@gmail.com>
2023-04-13 00:17:25 -0300

    :sparkles: feat: Versão mobile.

--- src/components/Section.vue
+++ src/components/Section.vue
@@ -102,25 +102,25 @@ export default {
 </script>
 
 <template>
-    <div class="container mx-auto pt-10">
+    <div class="pt-24 container mx-auto md:pt-10 md-auto ">
         <!-- Home -->
         <section id="home">
-            <div class="grid justify-items-stretch content-center h-full w-11/12 space-y-2">
-                <p class="text-lt font-['SFMono-Regular']">Hi, my name is</p>
-                <h1 class="text-blue-lt text-7xl font-bold font-['Calibre 500'] pt-3">Diêgo Correia de Andrade.</h1>
-                <h3 class="text-blue-ft text-7xl font-bold font-['Calibre 500']">I build things for the web.</h3>
-                <h6 class="text-blue-ft text-1xl text-justify font-['Calibre 300'] w-9/12 tracking-[.25em] leading-relaxed py-8">I am a software developer specialized in building and designing digital solutions and experiences. Currently, 
+            <div class="grid justify-items-stretch content-center h-full w-12/12 space-y-2 md:w11/12 md:pt-10 pt-14">
+                <p class="text-lt font-['SFMono-Regular'] pl-5">Hi, my name is</p>
+                <h1 class="text-blue-lt md:text-7xl font-bold font-['Calibre 500'] pt-3 text-2xl pl-5">Diêgo Correia de Andrade.</h1>
+                <h3 class="text-blue-ft md:text-7xl font-bold font-['Calibre 500'] text-2xl pl-5">I build things for the web.</h3>
+                <h6 class="text-blue-ft md:text-1xl text-justify font-['Calibre 300'] w-12/12 md:w-9/12 tracking-[.25em] leading-relaxed py-8 text-0.5xl pl-5 pr-3">I am a software developer specialized in building and designing digital solutions and experiences. Currently, 
                     I am focused on building accessible and transformative products for the society at 
                     <span class="underline"><a class="hover:text-blue-lt cursor-pointer" href="https://www.dortech-group.com" target="_blank">Dortech</a></span>.</h6>
-                <div class="pt-10">
-                    <button class="bg-transparent text-lt border-lt rounded w-3/12 hover:text-blue-lt hover:border-blue-lt">Discover my articles</button>
+                <div class="md:pt-10 pl-5">
+                    <button class="bg-transparent text-lt border-lt rounded w-12/12 md:w-3/12 hover:text-blue-lt hover:border-blue-lt">Discover my articles</button>
                 </div>
             </div>
         </section>
         <!-- Skills -->
         <section id="skill">
-            <div class=" grid justify-items-stretch content-center h-full w-10/12 space-y-2">
-                <p class="text-lt font-['SFMono-Regular']">My skills are...</p>
+            <div class=" grid justify-items-stretch content-center h-full w-10/12 space-y-2 md:pt-10 pt-24">
+                <p class="text-lt font-['SFMono-Regular'] pl-5">My skills are...</p>
                 <div v-show="mediaFrontend() > 0" class="flex flex-row flex-justify-between w-12/12 ml-10 pt-10">
                     <div class="w-6/12 "><span @click="toggleFrontend" class="cursor-pointer text-blue-lt text-3xl font-semibold font-['Calibre 500']">Frontend</span></div>
                     <progress class="w-6/12" :value="mediaFrontend()" max="100">{progressFronted}%</progress>
@@ -205,14 +205,14 @@ export default {
         </section>
         <!-- work -->
         <section id="work">
-            <div class="grid justify-items-stretch content-center h-screen w-12/12 space-y-5 pt-40">
-                <p class="text-lt font-['SFMono-Regular'] pb-5">These are my works,</p>
-                <div class="flex flex-row w-12/12 h-50 pb-5">
+            <div class="grid justify-items-stretch content-center md:h-screen h-full w-12/12 space-y-5 md:pt-40 pt-24">
+                <p class="text-lt font-['SFMono-Regular'] md:pb-5 pl-5">These are my works,</p>
+                <div class="flex flex-row w-12/12 md:h-50 pb-5">
                     <div class="w-1/12"></div>
-                    <div class="flex flex-col space-y-6 w-6/12">
-                        <h1 class="text-blue-lt font-bold text-3xl font-['Calibre 500']">Dortech Group</h1>
-                        <p class="text-blue-ft font-semibold text-xl font-['Calibre 300']">Website created for validation and promotion of the brand and of the services provided by the company.</p>
-                        <a href="https://www.dortech-group.com/" class="pt-10" target="_blank"><span class="text-blue-ft font-semibold text-xs hover:text-lt">View here</span></a>
+                    <div class="flex flex-col md:space-y-6 w-6/12 md:pl-0 pr-5 space-y-3">
+                        <h1 class="text-blue-lt font-bold md:text-3xl font-['Calibre 500'] text-lg">Dortech Group</h1>
+                        <p class="text-blue-ft font-semibold md:text-xl font-['Calibre 300'] text-sm">Website created for validation and promotion of the brand and of the services provided by the company.</p>
+                        <a href="https://www.dortech-group.com/" class="md:pt-10" target="_blank"><span class="text-lt font-semibold text-xs hover:text-blue-ft">View here</span></a>
                     </div>
                     
                     <img src="https://img.dortech-group.com/leque_04.png" alt="" class="flex object-scale-down object-right w-4/12 img-work">
@@ -220,53 +220,48 @@ export default {
                 
                 <div class="flex flex-row content-center w-12/12">
                     <div class="w-1/12"></div>
-                    <img src="https://img.dortech-group.com/LOGO3DX1.png" alt="" class="flex object-scale-down object-left w-4/12 img-work">
-                    <div class="flex flex-col space-y-6 w-6/12">
-                        <h1 class="text-blue-lt font-bold text-3xl font-['Calibre 500']">GO3DX</h1>
-                        <p class="text-blue-ft font-semibold text-xl font-['Calibre 300']">Teaching and validation platform focused on the development of product designers for the industry.</p>
-                        <a href="https://www.go3dx.co/" class="pt-10" target="_blank"><span class="text-blue-ft font-semibold text-xs hover:text-lt">View here</span></a>
+                    <img src="https://img.dortech-group.com/LOGO3DX1.png" alt="" class="flex object-scale-down object-left md:w-4/12 img-work w-3/12 ">
+                    <div class="flex flex-col md:space-y-6 w-6/12 md:pl-0 pl-5 space-y-3">
+                        <h1 class="text-blue-lt font-bold md:text-3xl font-['Calibre 500'] text-lg">GO3DX</h1>
+                        <p class="text-blue-ft font-semibold md:text-xl font-['Calibre 300'] text-sm">Teaching and validation platform focused on the development of product designers for the industry.</p>
+                        <a href="https://www.go3dx.co/" class="md:pt-10 pt-0" target="_blank"><span class="text-lt font-semibold text-xs hover:text-blue-ft">View here</span></a>
                     </div>
                 </div>
             </div>
         </section>
         <!-- contact -->
         <section id="contact">
-            <div class="grid items-stretch content-center h-screen w-12/12 space-y-2">
-                <p class="text-lt font-['SFMono-Regular']">And to contact me,</p>
-                <div class="grid justify-items-center h-90 w-full place-content-center pt-20">
+            <div class="grid items-stretch content-center md:h-screen h:full w-12/12 space-y-2 md:pt-0 pt-14 pb-10">
+                <p class="text-lt font-['SFMono-Regular'] pl-5">And to contact me,</p>
+                <div class="grid justify-items-center md:h-90 w-full place-content-center md:pt-20 pt-10 h-full">
                     <div class="grid grid-cols-3 w-12/12">
                         <div class="col-start-1 col-end-1 w-12/12">
-                                <img class="cursor-pointer" :src="[whatsApp ? '/src/assets/image/wpp-select.svg': '/src/assets/image/wpp.svg']" @mouseover="changeColor('whatsApp')"  @mouseleave="changeColor('whatsApp')" @click="callMe('whatsApp')">
-                                
+                                <img class="cursor-pointer flex object-scale-down social" :src="[whatsApp ? './assets/image/wpp-select.svg': './assets/image/wpp.svg']" @mouseover="changeColor('whatsApp')"  @mouseleave="changeColor('whatsApp')" @click="callMe('whatsApp')">
                         </div>
                         
                         <div class="col-start-3 col-end-3 flex flex-row justify-end">
-                            <img class="cursor-pointer" :src="[linkedin ? '/src/assets/image/lk-select.svg': '/src/assets/image/lk.svg']"  @mouseover="changeColor('linkedin')"  @mouseleave="changeColor('linkedin')"  @click="callMe('linkedin')">
+                            <img class="cursor-pointer flex object-scale-down social" :src="[linkedin ? './assets/image/lk-select.svg': './assets/image/lk.svg']"  @mouseover="changeColor('linkedin')"  @mouseleave="changeColor('linkedin')"  @click="callMe('linkedin')">
                         </div>
                         <div class="col-start-2 col-end-2 photo">
                             <img src="../assets/image/Foto-perfil.jpeg" alt="" srcset="" class="profile-image object-cover rounded-full border-2 self-center" :class="[img ? 'border-lt':'border-blue-ft']" @mouseover="changeColor()"  @mouseleave="changeColor()">
                         </div>
                         <div class="col-start-1 col-end-1">
-                            <img class="cursor-pointer" :src="[gmail ? '/src/assets/image/gmail-select.svg': '/src/assets/image/gmail.svg']"  @mouseover="changeColor('gmail')"  @mouseleave="changeColor('gmail')" @click="callMe('gmail')">
+                            <img class="cursor-pointer flex object-scale-down social" :src="[gmail ? './assets/image/gmail-select.svg': './assets/image/gmail.svg']"  @mouseover="changeColor('gmail')"  @mouseleave="changeColor('gmail')" @click="callMe('gmail')">
                         </div>
                         <div class="col-start-3 col-end-3 flex flex-row justify-end">
-                            <img class="cursor-pointer" :src="[telegram ? '/src/assets/image/tg-select.svg': '/src/assets/image/tg.svg']"  @mouseover="changeColor('telegram')"  @mouseleave="changeColor('telegram')" @click="callMe('telegram')">
+                            <img class="cursor-pointer flex object-scale-down social" :src="[telegram ? './assets/image/tg-select.svg': './assets/image/tg.svg']"  @mouseover="changeColor('telegram')"  @mouseleave="changeColor('telegram')" @click="callMe('telegram')">
                         </div>
                     </div>
                 </div>
             </div>
         </section>
-    </div>
+    </div> 
   
 </template>
 
 <style>
-  html {
-    overflow: hidden;
-  }
-  section{
-    height: 100vh;
-  }
+
+
   progress[value]::-webkit-progress-bar{
     background-color: rgba(136, 146, 176, 0.55);
     width: 100%;
@@ -286,8 +281,26 @@ export default {
   }
   
   .profile-image{
-    width: 20vh;
-    height: 20vh;
+    width: 10vh;
+    height: 10vh;
   }
+
+  .social{
+    width: 5vh;
+    height: 5vh;
+  }
+
+@media (min-width: 640px) {
+    html {
+    overflow: hidden;
+  }
+  section{
+    height: 100vh;
+  }
+  .profile-image{
+    width: 25vh;
+    height: 25vh;
+  }
+}
 
 </style>
--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -41,31 +41,31 @@ export default{
 </script>
 <template>
     <nav class="bg-first-blue fixed w-full shadow-md md:drop-shadow-lg px-2 sm:px-4 py-2.5 dark:bg-first-blue">
-  <div class="container flex flex-wrap items-center justify-between mx-auto">
-    <a href="/" class="flex items-center">
-      <img src="../assets/image/logo.svg" alt="">
-    </a>
-    <div class="hidden w-auto md:block md:w-auto" id="navbar-default">
-      <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-first-blue dark:border-gray-700">
-        <li>
-          <a href="#home" class="block py-2 pl-3 pr-4  text-white md:bg-transparent rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" :class="[home ? 'md:text-lt':'text-white md:text-white']"  @click="toggleHome()"><span>I.</span>Home</a>
-        </li>
-        <li>
-          <a href="#skill" class="block py-2 pl-3 pr-4  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" :class="[skill ? 'text-lt md:text-lt':'text-white md:text-white']" @click="toggleSkill()"><span>II.</span>Experience</a>
-        </li>
-        <li>
-          <a href="#work" class="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" :class="[work ? 'text-lt md:text-lt':'text-white md:text-white']" @click="toggleWork()"><span>III.</span>Work</a>
-        </li>
-        <li>
-          <a href="#contact" class="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" :class="[contact ? 'text-lt md:text-lt':'text-white md:text-white']" @click="toggleContact()"><span>IV.</span>Contact</a>
-        </li>
-        <li>
-          <button class="block py-2 pl-3 pr-4 text-lt md:text-lt md:bg-transparent md:border-2 md:border-lt md:hover:border-white md:hover:text-white" @click="openResume()">Resume</button>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+      <div class="container flex md:flex-wrap md:items-center md:justify-between justify-center mx-auto w-12/12">
+        <a href="/" class="flex">
+          <img src="../assets/image/logo.svg" alt="">
+        </a>
+        <div class="hidden w-auto md:block md:w-auto" id="navbar-default">
+          <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-first-blue dark:border-gray-700">
+            <li>
+              <a href="#home" class="block py-2 pl-3 pr-4  text-white md:bg-transparent rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" :class="[home ? 'md:text-lt':'text-white md:text-white']"  @click="toggleHome()"><span>I.</span>Home</a>
+            </li>
+            <li>
+              <a href="#skill" class="block py-2 pl-3 pr-4  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" :class="[skill ? 'text-lt md:text-lt':'text-white md:text-white']" @click="toggleSkill()"><span>II.</span>Experience</a>
+            </li>
+            <li>
+              <a href="#work" class="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" :class="[work ? 'text-lt md:text-lt':'text-white md:text-white']" @click="toggleWork()"><span>III.</span>Work</a>
+            </li>
+            <li>
+              <a href="#contact" class="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" :class="[contact ? 'text-lt md:text-lt':'text-white md:text-white']" @click="toggleContact()"><span>IV.</span>Contact</a>
+            </li>
+            <li>
+              <button class="block py-2 pl-3 pr-4 text-lt md:text-lt md:bg-transparent md:border-2 md:border-lt md:hover:border-white md:hover:text-white" @click="openResume()">Resume</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
 </template>
 
 <style>
